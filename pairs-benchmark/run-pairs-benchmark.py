363897cb3967f9c5f75d2b5757d9105f9d0c03d0
Stefano Balietti <futur.dorko@gmail.com>
2013-09-15 12:40:35 +0200

    local changes

--- pairs-benchmark/run-pairs-benchmark.py
+++ pairs-benchmark/run-pairs-benchmark.py
@@ -12,7 +12,7 @@ import subprocess as sub
 import numpy as np
 
 # CHANGE THIS FOR YOUR SETUP:
-nodegame_path =                 '../../nodegame/'
+nodegame_path =                 '../../ngtest/'
 server_path   = nodegame_path + 'node_modules/nodegame-server/'
 msglog_path   = server_path   + 'log/messages'
 
