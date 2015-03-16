896b3ce32b113dce04ac14af33ef355c169145de
jdoerrie <jwdoerrie@gmail.com>
2015-03-16 09:54:07 -0500

    Added more metrics, improved documentation and log stdout and stderr to files

--- experiment_stats/run_experiment.py
+++ experiment_stats/run_experiment.py
@@ -8,17 +8,25 @@ import subprocess
 import time
 import csv
 import psutil
+import platform
 from argparse import ArgumentParser
 from collections import Counter
 
 
 def write_num_games(num_games, cfg):
+    """ Writes the number of games `num_games` to the `game_settings_file`
+    as specified in the configuration file.
+    """
     regex = re.compile(r'({0})\W*:\W*\d+'.format(cfg['num_games_keyword']))
     for line in fileinput.input(cfg['game_settings_file'], inplace=True):
         print regex.sub(r'\1: ' + str(num_games), line.rstrip())
 
 
 def write_timeout(timeout, reliable, cfg):
+    """ Writes the retry timeout and the reliable boolean flag to the
+    `variables_file`. Note that even though timeout is written every time it
+    only takes effect if reliable == True.
+    """
     regex_reliable = re.compile(r'({0})\W*=\W*(true|false)'.
                                 format(cfg['rel_msgs_var']))
 
@@ -35,24 +43,48 @@ def write_timeout(timeout, reliable, cfg):
 
 
 def run_launcher(cfg):
-    with open(os.devnull, 'w') as dev_null:
+    """ Executes `node launcher.js` from the right cwd and logs stdout and
+    stderr to a previously defined folder.
+    """
+    stdout_log = os.path.join(cfg['exp_log_dir'],
+                              'experiment_{0}_stdout.log'.
+                              format(cfg['exp_time']))
+
+    stderr_log = os.path.join(cfg['exp_log_dir'],
+                              'experiment_{0}_stderr.log'.
+                              format(cfg['exp_time']))
+
+    with open(stdout_log, 'a') as f_out, open(stderr_log, 'a') as f_err:
         node_proc = subprocess.Popen(['node', cfg['launcher_file']],
                                      cwd=cfg['launcher_dir'],
-                                     stdout=dev_null,
-                                     stderr=subprocess.STDOUT)
-        p = psutil.Process(node_proc.pid)
-        while node_proc.poll() == None:
-            print p.cpu_times()
-            time.sleep(5)
-        retcode = node_proc.wait()
-        return retcode, node_proc.pid
+                                     stdout=f_out,
+                                     stderr=f_err)
+
+        return node_proc
+
+
+def get_process_metrics(proc):
+    """ Extracts cpu times, memory infos and connection infos about a given
+    process started via Popen(). Also obtains the return code.
+    """
+    p = psutil.Process(proc.pid)
+    while proc.poll() == None:
+        cpu, mem, conns = p.cpu_times(), p.memory_info(), p.connections('all')
+        time.sleep(1)
+    retcode = proc.wait()
+
+    return retcode, cpu, mem, conns
 
 
 def run_test(cfg):
-        return subprocess.call(['npm', 'test'], cwd=cfg['test_dir'])
+    """ Runs `npm test` from the correct cwd and returns the return code. """
+    return subprocess.call(['npm', 'test'], cwd=cfg['test_dir'])
 
 
 def parse_server_msg_file(msg_file):
+    """ Parses the server message log file. Extract metrics about the total
+    number of messages and the break down according to type.
+    """
     msg_counter = Counter()
     with open(msg_file) as messages:
         for message in messages:
@@ -66,6 +98,7 @@ def parse_server_msg_file(msg_file):
 
 
 def main():
+    # Define ArgumentParser and declare all needed command line arguments
     parser = ArgumentParser(description='Execute nodegame experiment and write'
                             ' experiment data to csv file.')
 
@@ -87,9 +120,10 @@ def main():
 
     args = parser.parse_args()
 
+    # Manually check dependency of reliable on timeouts
     if args.reliable and not args.timeouts:
-        print('Error: --timeouts need to be specified when reliable messaging '
-              'is activated.')
+        print('Error: --timeouts needs to be specified when reliable messaging'
+              ' is activated.')
         return
 
     with open(args.config) as cfg_fp:
@@ -98,19 +132,29 @@ def main():
     if not args.reliable:
         args.timeouts = [cfg["default_timeout"]]
 
-    exp_time = int(time.time() * 10**6)  # time with micro-second accuracy
+    # record the current unix time in micro seconds and store it in cfg
+    cfg['exp_time'] = int(time.time() * 10**6)
+
+    # construct metrics.csv file name
     csv_metrics_file = os.path.join(cfg['csv_out_dir'],
                                     'experiment_{0}_metrics.csv'.
-                                    format(exp_time))
+                                    format(cfg['exp_time']))
 
+    # construct messages.csv file name
     csv_msg_file = os.path.join(cfg['csv_out_dir'],
-                                'experiment_{0}_messages.csv'.format(exp_time))
+                                'experiment_{0}_messages.csv'.
+                                format(cfg['exp_time']))
 
-    with open(csv_msg_file, 'w') as csv_msg:
-        exp_metric_names = [
-            "id", "nConn", "is_reliable", "timeout", "nACKs", "machine",
-            "total_msgs", "test_result"
+    with open(csv_metrics_file, 'w') as csv_metrics, \
+         open(csv_msg_file, 'w') as csv_msg:
+        metrics_names = [
+            "id", "machine", "num_conns", "is_reliable", "timeout",
+            "exp_ret_code", "test_ret_code", "cpu_time_user",
+            "cpu_time_system", "mem_info_rss", "mem_info_vms"
         ]
+
+        metrics_writer = csv.DictWriter(csv_metrics, fieldnames=metrics_names)
+        metrics_writer.writeheader()
 
         msg_names = [
             "id", "total",
@@ -130,13 +174,39 @@ def main():
                 write_timeout(timeout, bool(args.reliable), cfg)
                 server_msg = os.path.join(cfg["msg_log_dir"],
                                           cfg["server_msg_file"])
-                os.remove(server_msg)
+                try:
+                    os.remove(server_msg)
+                except OSError:
+                    pass
+
+                # run_time serves as the current run id, again unix timestamp
                 run_time = int(time.time() * 10**6)
-                code, pid = run_launcher(cfg)
-                run_test(cfg)
+
+                proc = run_launcher(cfg)
+                ret_exp, cpu, mem, conns = get_process_metrics(proc)
+
+                ret_test = run_test(cfg)
+
+                exp_metrics = {
+                    'id': run_time,
+                    'machine': platform.platform(),
+                    'num_conns': num_games,
+                    'is_reliable': bool(args.reliable),
+                    'timeout': timeout,
+                    'exp_ret_code': ret_exp,
+                    'test_ret_code': ret_test,
+                    'cpu_time_user': cpu[0],
+                    'cpu_time_system': cpu[1],
+                    'mem_info_rss': mem[0],
+                    'mem_info_vms': mem[1]
+                }
+
+                metrics_writer.writerow(exp_metrics)
                 msg_counter = parse_server_msg_file(server_msg)
 
                 msg_counter["id"] = run_time
+                # we manually set not occurring counts to 0 to avoid empty
+                # strings in the csv
                 for msg_name in msg_names:
                     if msg_name not in msg_counter:
                         msg_counter[msg_name] = 0
